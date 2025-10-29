1a0ed3b24f2efa26db9b06929a6c131f3183ecb1
tuduri11 <toni.t.m@hotmail.com>
2025-10-29 02:27:07 +0100

    Cambio nombre función

--- app/services/importer.py
+++ app/services/importer.py
@@ -17,7 +17,7 @@ def import_feed(csv_path: str):
     except Exception as e:
         log.error(f"Error reading or processing CSV: {csv_path}, Error: {e}")
         raise 
-    
+
     with engine.begin() as conn:
         for fila in df.to_dict(orient="records"):
             pid = int(fila["product_id"])
@@ -29,7 +29,7 @@ def import_feed(csv_path: str):
 
         for pid, group in df.groupby("product_id"):
             allowed_store_ids = set(group["store_id"].astype(int)) 
-            store_ids_to_remove = get_existing_store_ids(conn, pid) - allowed_store_ids
+            store_ids_to_remove = get_store_ids(conn, pid) - allowed_store_ids
             if store_ids_to_remove:
                 for sid in store_ids_to_remove:
                     remove_listing(conn, pid, sid)
